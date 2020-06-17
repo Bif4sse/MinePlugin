eb12bd96d115a0afa843e90bb7fba615b3f8e22f
Bif4sse <fabis.anthony@gmail.com>
2020-06-17 21:46:31 +0200

    Première initialisation

---
+++ src/fr/ultima/ultimafirsttry/Main.java
@@ -0,0 +1,17 @@
+package fr.ultima.ultimafirsttry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		System.out.println("Bitenbois");
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("Ok c'est bon j'arrete");
+	}
+
+}
